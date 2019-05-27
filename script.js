7e45e431fcd2684a3e6ea455d139b68adec09efe
Saif Ali <saif_ali@mcafee.com>
2019-05-27 03:50:30 -0700

    Added level information to alerts.

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ var arr = [];
 var hidden = true;
 var numOfCircles = 5;
 var numOfSides = 6;
+var level = 1;
+
 
 function randomInRange(start,end){
        return Math.floor(Math.random() * (end - start + 1) + start);
 }
+
+
+function createRandomArray() {
+  for(var i=1; i<=numOfCircles;) {
+    var random = randomInRange(1,numOfSides*numOfSides);
+    if(arr.includes(random)) {
+      continue;
+    }
+    arr.push(random);
+    i++;
+  }
+  console.log(arr);
+  return arr;
+}
+
 
 function setCircles() {
 	hideCircles();
@@ -15,7 +32,6 @@ function setCircles() {
 		console.log(displacement);
 		$("#"+arr[i]).find(".circle").css("top",displacement+"px");
 		$("#"+arr[i]).find(".circle").css("left",displacement+"px");
-
 		$("#"+arr[i]).find(".circle").show();
 	}
 	hidden = false;
@@ -25,9 +41,10 @@ function hideCircles() {
 	$(".circle").hide();
 	hidden=true;
 }
-function createGrid(side) { 
-	var grid_id = 1;
 
+
+function createGrid(side) {
+	var grid_id = 1;
 	for(var row=0; row<side; row++){
 		for( var col=0; col<side; col++) {
 			$("#container").append("<div class='grid' id='"+grid_id+"'><div class='circle'></div></div>");
@@ -41,6 +58,7 @@ function createGrid(side) {
 	$(".circle").hide();
 }
 
+
 function isCircleSet(clicked) {
 	for( var i=0;i<arr.length;i++) {
 		if(clicked == arr[i]){
@@ -52,39 +70,22 @@ function isCircleSet(clicked) {
 
 function resetGame() {
 	numOfCircles = 5;
+  level = 1;
 	alert("Game Over. Try again!");
 	arr = [];
-	for(var i=1; i<=numOfCircles;){
-		var random = randomInRange(1,numOfSides*numOfSides);
-		if(arr.includes(random)) {
-			continue;
-		}
-		arr.push(random);
-		i++;
-	}
-	console.log(arr);
+	arr = createRandomArray();
 	setCircles(arr);
-
 }
 
 function nextLevel(){
-	console.log("Next level.");
+	console.log("Next level = "+level);
 	numOfCircles+=1;
-	arr = [];
-	for(var i=1; i<=numOfCircles;){
-		var random = randomInRange(1,36);
-		if(arr.includes(random)) {
-			continue;
-		}
-		arr.push(random);
-		i++;
-	}
-	console.log(arr);
+  level+=1;
+	arr = createRandomArray();
 	setCircles(arr);
 }
 
 function progressGame(clicked) {
-
 	console.log("Continuing...");
 	if(hidden==false) {
 		hideCircles();
@@ -94,41 +95,30 @@ function progressGame(clicked) {
 	console.log(index);
 	arr.splice(index,1);
 	if(arr.length==0) {
-		alert("You won!");
+		alert("You have cleared level "+level+"!");
 		nextLevel();
 		return arr;
 	}
-	console.log(arr);	
+	console.log(arr);
 	return arr;
-
-} 
+}
 
 $(document).ready(function() {
 	console.log("Ready!");
 	createGrid(numOfSides);
-	for(var i=1; i<=numOfCircles;){
-		var random = randomInRange(1,numOfSides*numOfSides);
-		if(arr.includes(random)) {
-			continue;
-		}
-		arr.push(random);
-		i++;
-	}
-	console.log(arr);
+  arr = createRandomArray();
 	setCircles(arr);
 
 	$(".grid").click(function () {
-	let clicked = ($(this).attr('id'));
-	clicked = parseInt(clicked);
-	console.log(clicked);
+  	let clicked = ($(this).attr('id'));
+  	clicked = parseInt(clicked);
+  	console.log(clicked);
 
-	if(!isCircleSet(clicked)){
-		resetGame();
-	} else {
-		arr = progressGame(clicked);
-	}
+  	if(!isCircleSet(clicked)){
+  		resetGame();
+  	} else {
+  		arr = progressGame(clicked);
+  	}
+  });
 
 });
-
-});
-
